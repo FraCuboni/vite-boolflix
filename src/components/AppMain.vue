46bafd5827a04949239dbce90514441b64be18a7
FraCuboni <fracuboni@gmail.com>
2024-07-11 12:48:44 +0200

    feat card component and AppMain component created

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,11 +13,25 @@ export default{
 </script>
 
 <template>
-    <div class="container"></div>
+    <div class="container">
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+        <Card/>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
 @use '../styles/partials/mixins' as *;
 @use '../styles/partials/variables' as *;
+
+.container{
+    background-color: $tertiary_c;
+    padding: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -7,10 +7,32 @@ export default{
 </script>
 
 <template>
+    <div class="box">
+        <div class="imgbox"></div>
+        <p>title</p>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
 @use '../styles/partials/mixins' as *;
 @use '../styles/partials/variables' as *;
+
+.box{
+    border: 1px solid white;
+    width: calc(100% / 5 - 10px);
+
+    .imgbox{
+        background-color: red;
+        width: 100%;
+        height: 20px;
+    }
+
+    p{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: white;
+    }
+}
 </style>
